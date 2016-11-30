daf23e8d5fd388e53129735b9bb97ab263ea5d85
zhao_hongsheng <zhao_hongsheng@microad-tech.com>
2016-11-30 16:53:08 +0800

    character walk

--- rpg/frontend/maps.js
+++ rpg/frontend/maps.js
@@ -23,18 +23,72 @@ maps["mapMapping"] = {
 	}
 }
 
-maps["01"] =
-"tr,tr,tr,be,se,br,se,be,tr,tr,tr_" +
-"tr,tr,tr,be,se,br,se,be,tr,tr,tr_" +
-"tr,tr,tr,be,be,br,be,be,tr,tr,tr_" +
-"tr,tr,tr,la,la,la,la,la,tr,tr,tr_" +
-"tr,tr,tr,la,la,la,la,la,tr,tr,tr_" +
-"la,la,la,la,la,la,la,la,la,la,la_" +
-"tr,tr,tr,la,la,la,la,la,tr,tr,tr_" +
-"tr,tr,tr,la,la,la,la,la,tr,tr,tr_" +
-"tr,tr,tr,be,be,br,be,be,tr,tr,tr_" +
-"tr,tr,tr,be,se,br,se,be,tr,tr,tr_" +
-"tr,tr,tr,be,se,br,se,be,tr,tr,tr";
+maps["01"] = {
+	data: 
+		"tr,tr,tr,be,se,br,se,be,tr,tr,tr_" +
+		"tr,tr,tr,be,se,br,se,be,tr,tr,tr_" +
+		"tr,tr,tr,be,be,br,be,be,tr,tr,tr_" +
+		"tr,tr,tr,la,la,la,la,la,tr,tr,tr_" +
+		"tr,tr,tr,la,la,la,la,la,tr,tr,tr_" +
+		"la,la,la,la,la,la,la,la,la,la,la_" +
+		"tr,tr,tr,la,la,la,la,la,tr,tr,tr_" +
+		"tr,tr,tr,la,la,la,la,la,tr,tr,tr_" +
+		"tr,tr,tr,be,be,br,be,be,tr,tr,tr_" +
+		"tr,tr,tr,be,se,br,se,be,tr,tr,tr_" +
+		"tr,tr,tr,be,se,br,se,be,tr,tr,tr",
+	exit: {
+		"10_5": {
+			map: "02",
+			initPosition: "0_5"
+		},
+		"5_10": {
+			map: "03",
+			initPosition: "1_0"
+		}
+	}
+};
+
+maps["02"] = {
+	data: 
+		"tr,tr,tr,tr,tr,tr,tr,tr,tr,tr,tr_" +
+		"tr,tr,tr,tr,tr,tr,tr,tr,tr,tr,tr_" +
+		"tr,tr,tr,tr,tr,tr,tr,tr,tr,tr,tr_" +
+		"tr,tr,tr,tr,tr,tr,tr,tr,tr,tr,tr_" +
+		"la,la,la,la,la,la,la,la,la,la,la_" +
+		"la,la,la,la,la,la,la,la,la,la,la_" +
+		"la,la,la,la,la,la,la,la,la,la,la_" +
+		"tr,tr,tr,tr,tr,tr,tr,tr,tr,tr,tr_" +
+		"tr,tr,tr,tr,tr,tr,tr,tr,tr,tr,tr_" +
+		"tr,tr,tr,tr,tr,tr,tr,tr,tr,tr,tr_" +
+		"tr,tr,tr,tr,tr,tr,tr,tr,tr,tr,tr_",
+	exit: {
+		"0_5": {
+			map: "01",
+			initPosition: "10_5"
+		}
+	}
+};
+
+maps["03"] = {
+	data: 
+		"tr,la,la,tr,tr,tr,tr,tr,tr,tr,tr_" +
+		"tr,tr,la,la,tr,tr,tr,tr,tr,tr,tr_" +
+		"tr,tr,tr,la,la,tr,tr,la,la,la,tr_" +
+		"tr,tr,tr,tr,la,la,tr,la,tr,la,tr_" +
+		"tr,tr,tr,tr,tr,la,tr,la,tr,la,tr_" +
+		"tr,tr,tr,tr,tr,la,tr,la,tr,la,tr_" +
+		"tr,tr,tr,tr,tr,la,tr,la,tr,la,tr_" +
+		"tr,tr,tr,tr,tr,la,tr,la,tr,la,tr_" +
+		"tr,tr,tr,tr,tr,la,la,la,la,la,tr_" +
+		"tr,tr,tr,tr,tr,tr,tr,la,tr,tr,tr_" +
+		"tr,tr,tr,tr,tr,tr,tr,la,la,la,tr_",
+	exit: {
+		"1_0": {
+			map: "01",
+			initPosition: "5_10"
+		}
+	}
+};
 
 
 // 地图数据串转二维数组
@@ -50,7 +104,7 @@ function toArray(str){
 
 for(var key in maps){
 	if(/\d\d/.test(key)){
-		maps[key] = toArray(maps[key]);
+		maps[key].data = toArray(maps[key].data);
 	}
 }
 
